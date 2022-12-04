cade86f8093f43f16e8ef2a92c073a6ed25ce796
Chandresh Kumar <chandresh.code@gmail.com>
2022-12-04 15:09:10 +0530

    feat: config changes

--- commons/config/config.py
+++
@@ -1,1 +0,0 @@
-RESOURCES_DIR = 'C:/Users/Khatri/Desktop/work/Code/projects/t-axis-media/Development/Resources'
--- commons/config_loader.py
+++ commons/config_loader.py
@@ -1,4 +1,4 @@
 import configparser
 
 config_parser = configparser.ConfigParser()
-config_parser.read('./commons/config.local.ini')
+config_parser.read('./commons/config.ini')
